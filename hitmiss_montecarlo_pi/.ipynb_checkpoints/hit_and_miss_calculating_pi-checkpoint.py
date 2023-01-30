c5e876071bdf1fa2e2fa3c1a939b7da09ca0872f
Mamba <mumbo1002@gmail.com>
2023-01-30 13:32:38 -0600

    Large % Error

--- hitmiss_montecarlo_pi/.ipynb_checkpoints/hit_and_miss_calculating_pi-checkpoint.py
+++ hitmiss_montecarlo_pi/.ipynb_checkpoints/hit_and_miss_calculating_pi-checkpoint.py
@@ -4,32 +4,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
-# import cprofile
  
 def plot():
     sb.set(style='whitegrid', rc={"figure.figsize":(4, 4)})    
-    sb.set_palette(['#EF476F', '#62C370'])
+    sb.set_palette(['#62C370', '#EF476F'])
 
     fig = plt.figure()
-    x1 = fig.add_subplot(111, projection='3d')
+    x1 = fig.add_subplot(projection='3d')
+    # x1.set_aspect("equal")
 
-    x1.view_init(45, 45)
+    # x1.view_init(45, 55)
 
+    # Draw Scatter
     for s in df.in_bounds.unique():
         x1.scatter(df.x[df.in_bounds==s],df.y[df.in_bounds==s],df.z[df.in_bounds==s],label=s)
-
-    # plot/seaborn settings
-    x1.set(xlim=(-1.02, 1.02))
-    x1.set(ylim=(-1.02, 1.02))
-    x1.legend([],[], frameon=False)
-
+    
+    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+    x = np.cos(u)*np.sin(v)
+    y = np.sin(u)*np.sin(v)
+    z = np.cos(v)
+    x1.plot_wireframe(x, y, z, color="b")
+    
     plt.show()
+    
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i", "--iterations", help="Iteration count.", type=int, default=100)
+parser.add_argument("-i", "--iterations", help="Iteration count.", type=int, default=1000)
 parser.add_argument("-s", "--seed", help="Random generation seed.", type=int, default=2)
-parser.add_argument("-p", "--plot", help="Plot Results (True/False).", type=bool, default=False)
+parser.add_argument("-p", "--plot", help="Plot Results (True/False).", action="store_true")
 args = parser.parse_args()
 
 np.random.seed(args.seed)
@@ -38,11 +41,6 @@ df = pd.DataFrame(columns =["x","y","in_bounds","radius","one_minus_radius"])
 # Pandas screams at us for not casting the column to a bool type for some reason? This fixes that even though it causes no issues... I miss type safety in rust :(
 df["in_bounds"]=df["in_bounds"].astype(bool)
 
-# main loop which:
-# 1. produces a point at uniformly random (x,y) coordinates
-# 2. calculates the distance from (0,0) to that point, as well as one minus that distance (for style)
-# 3. appends a new row to our dataframe, marking it as in or out of bounds
-# points which are at a distance greater than 1 (the radius of our circle) are known to be outside the circle
 index=0
 for e in range(0,  args.iterations):
     x=np.random.uniform(-1,1)
@@ -53,12 +51,17 @@ for e in range(0,  args.iterations):
     
     row_to_append = pd.DataFrame([{"x":x, "y":y, "z":z, "in_bounds": False if dist > 1 else True, "one_minus_radius": 1-dist, "radius": dist}])
     df = pd.concat([df,row_to_append])
-    df = pd.concat([df,row_to_append])
     
     index+=1
 
-mambajamba = df["in_bounds"].value_counts()[False]/ args.iterations
-print("Approximation result: ", mambajamba*4)
+percent = df["in_bounds"].value_counts()[False]/args.iterations
+calculated_pi = percent*6
 
-if args.plot == True:
+err = 6 * np.sqrt( (np.pi/6) * (1.0 - (np.pi/6) ))
+print("% in Sphere:", percent, "\nCalculated pi:", calculated_pi, "\nError:", err, "\n% Difference:", np.diff([calculated_pi, np.pi]))
+
+print("\n", df)
+
+if args.plot:
     plot()
+    
