edbdf8a73c98a5cea561e930d02fdc70bf3c6f3e
Mamba <mumbo1002@gmail.com>
2023-03-11 22:15:01 -0600

    updated Chisqfit.py

--- Problem 1/Chisqfit.py
+++ Problem 1/Chisqfit.py
@@ -1,63 +1,71 @@
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 import numpy as np
 from iminuit import Minuit
 from scipy.stats import chi2
+import argparse
+import seaborn as sb
+import pandas as pd
 
-# line model
-def line(x, c, m):
+def line_model(x, c, m):
     return m*x + c
     
-def custom_chisq(c, m):
+def custom_chisq_model(c, m):
     """ Chi-squared function to be minimized """
-    ym = line(x_data, c, m)
+    ym = line_model(x_data, c, m)
     z = (y_data - ym)/yerr_data
-#    print('ym = ',ym)
-#    print('z  = ',z)
     chisq = np.sum(z**2)
-    print('Chisq = ',chisq,'for c,m = ',c,m) 
+    # print('ym = ',ym)
+    # print('z  = ',z)
+    # print('Chisq = ',chisq,'for c,m = ',c,m) 
     return chisq
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--seed', type=int, default=2)
+parser.add_argument('-n', '--numpoints', type=int, default=20)
+parser.add_argument('-c', '--true_intercept', type=float, default=1.0)
+parser.add_argument('-sl', '--true_slope', type=float, default=2.0)
+args = parser.parse_args()
+
+
 # generate random toy data with random offsets in y 
-seed = 26
-np.random.seed(seed)
-print('seed',seed)
-x_data = np.linspace(-1.0, 1.0, 21)
+np.random.seed(args.seed)
+x_data = np.linspace(-1.0, 1.0, args.numpoints)
+ran = np.random.randn(len(x_data)) 
+y_true = line_model(x_data, args.true_intercept, args.true_slope)
 yerr_data = 0.1 + 0.05*x_data    # Make assumed Gaussian uncertainties increase with increasing x.
-c_true = 1.0
-m_true = 2.0
-y_true = line(x_data, c_true, m_true)
-ran = np.random.randn(len(x_data))
 y_data = y_true + yerr_data * ran  # Use standard normal random numbers
 
-print('x_data:',x_data)
-print('y_true:',y_true)
-print('ran:',ran)
-print('y_data:',y_data)
-print('yerr_data:',yerr_data)
+# This probably slows the script some but it puts everything in a nice table
+df_data = [x_data, y_data, y_true, yerr_data, ran]
+df = pd.DataFrame(dict(zip(range(len(df_data)), df_data)))
+df.columns = ["x", "y", "y-True", "y-Error", "Ran"] 
 
-# draw toy data
-plt.errorbar(x_data, y_data, yerr_data, fmt="o")
-
-m = Minuit(custom_chisq, c = 0.0, m = 0.0)  # starting values for intercept (c) and slope (m) parameters
-
+m = Minuit(custom_chisq_model, c = 0.0, m = 0.0)  # starting values for intercept (c) and slope (m) parameters
 m.errordef = 1.0 # Corresponds to change in function value associated with +- 1 standard error 
 m.migrad()  # finds minimum of least_squares function
 m.hesse()   # accurately computes uncertainties
 
-# draw data and fitted line
-plt.errorbar(x_data, y_data, yerr_data, fmt="o", label="Data")
-plt.plot(x_data, line(x_data, *m.values), label="Line fit")
 
-# display legend with some fit info
-fit_info = [
+# plot toy data
+sb.set_theme(style="darkgrid")
+fig = sb.FacetGrid(df).set(title="Chisqfit.py")
+sb.lineplot(x=x_data, y=line_model(x_data, *m.values), label="Line fit", color="m")
+fig.map(plt.errorbar, "x", "y", yerr=df["y-Error"], fmt="o", label="Data", color="m")
+
+legend = [
     f"$\\chi^2$ / $n_\\mathrm{{dof}}$ = {m.fval:.2f} / {len(x_data) - m.nfit}",
 ]
 for p, v, e in zip(m.parameters, m.values, m.errors):
-    fit_info.append(f"{p} = ${v:.3f} \\pm {e:.3f}$")
+    legend.append(f"{p} = ${v:.3f} \\pm {e:.3f}$")
+plt.legend(title="\n".join(legend))
 
-plt.legend(title="\n".join(fit_info))
 
-print('Best fit function value (Chi-squared):',m.fval,' for ndof = ',len(x_data)-m.nfit)
+print("Run Arguments:", args)
+print(df)
+
+print('\nBest fit function value (Chi-squared):',m.fval,' for ndof = ',len(x_data)-m.nfit)
 ndof = len(x_data)-m.nfit
 print('p-value =',1.0-chi2.cdf(m.fval, ndof))
 print('Fitted parameter values ',m.values)
@@ -65,5 +73,6 @@ print('Fitted parameter errors ',m.errors)
 print('Fitted parameter correlation coefficient matrix:')
 print(m.covariance.correlation())
 
-plt.show(block=False)
+
+plt.show()
 plt.pause(10)   # Keep on-screen for 10s
